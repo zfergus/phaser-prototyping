c4418885af767c23f9bb486e27e9465e251ad7b6
Zachary Ferguson <zfergus2@gmu.edu>
2015-02-08 03:39:37 -0500

    update 01

--- js/main.js
+++ js/main.js
@@ -6,54 +6,170 @@
  */
  
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
     "use strict";
+
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, "game", 
+		{ preload: preload, create: create, update: update } );
+
+	/* Tile map variable an layers */
+	var desert_tilemap;
+	var ground;
+	var shrubs;
+	var building1;
+	var building2;
+	var building3;	
+	var door1;
+	var door2;
+	var door3;	
+    var signs;
+	var tree;
+	
+	var player;
+	var player_speed = 256;
+	
+	/* Arrow key objects */
+	var cursors;
+	
+	function preload() {
+		/* Load tile map */
+		game.load.image("desert", "assets/desert_tiled.png");
+		game.load.image("desert_town", "assets/desert_town.png");
+		game.load.image("doors", "assets/door.png");
+		game.load.tilemap("desert_map", "assets/desert.json", null, Phaser.Tilemap.TILED_JSON);
+		/* Load sprites */
+		game.load.spritesheet("player", "assets/player.png", 34, 52);
+	}
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
+		/* Stretch out the world */
+		game.world.setBounds(0, 0, 32*40, 32*40);
+		
+		/* Create tile map */
+		desert_tilemap = game.add.tilemap("desert_map");
+		desert_tilemap.addTilesetImage("desert_tiled", "desert");
+		desert_tilemap.addTilesetImage("town1", "desert_town");
+		desert_tilemap.addTilesetImage("door", "doors");
+		ground = desert_tilemap.createLayer("ground");
+		shrubs = desert_tilemap.createLayer("shrub");
+		building1 = desert_tilemap.createLayer("building1");
+		building2 = desert_tilemap.createLayer("building2");
+		building3 = desert_tilemap.createLayer("building3");
+		door1 = desert_tilemap.createLayer("door1");
+		door2 = desert_tilemap.createLayer("door2");
+		door3 = desert_tilemap.createLayer("door3");
+		signs = desert_tilemap.createLayer("signs");
+		
+		desert_tilemap.setCollisionBetween(0, 175+48, true, building1);
+		desert_tilemap.setCollisionBetween(0, 175+48, true, building2);
+		desert_tilemap.setCollisionBetween(0, 175+48, true, building3);
+		var shrub_indexs = [52,53,58,60,61,137,138,139,140,145,146,147,148];
+		desert_tilemap.setCollision(shrub_indexs, true, shrubs);
+		desert_tilemap.setCollision(45, true, signs);
+		desert_tilemap.setCollision([224,225,226,227], true, door1);
+		desert_tilemap.setCollision([224,225,226,227], true, door2);
+		desert_tilemap.setCollision([224,225,226,227], true, door3);
+		
+		
+		
+		/* Create the player */
+		create_player();
+		/* Set the camera to follow the player */
+		game.camera.follow(player);
+		
+		tree = desert_tilemap.createLayer("tree");
+		
+		desert_tilemap.setCollision([150], true, tree);
+		
+		/* Create the arrow keys */
+		cursors = game.input.keyboard.createCursorKeys();
+		
+	}
     
+		/* Creates the players character sprite */
+	function create_player()
+	{
+		/* The player and its settings */
+		player = game.add.sprite(592, game.world.height-52, "player");
+	
+		/* Enable physics on the player */
+		game.physics.arcade.enable(player);
+	
+		/* Player physics properties. Give the little guy a slight bounce. */
+		player.body.collideWorldBounds = true;
+	
+		/* Add player animations */
+		player.animations.add( "left", [ 4,  5,  6,  7], 5, true);
+		player.animations.add("right", [ 8,  9, 10, 11], 5, true);
+		player.animations.add( "down", [ 1,  2,  3,  0], 5, true);
+		player.animations.add(   "up", [12, 13, 14, 15], 5, true);
+	}
+	
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
+		/**********************************************/
+		/************Collision and Overlaps************/  
+		/**********************************************/		
+		
+		/* Collision between player and building1 */
+		game.physics.arcade.collide(player, building1);
+		/* Collision between player and building2 */
+		game.physics.arcade.collide(player, building2);
+		/* Collision between player and building3 */
+		game.physics.arcade.collide(player, building3);
+		/* Collision between player and shrubs */
+		game.physics.arcade.collide(player, shrubs);
+		game.physics.arcade.collide(player, signs);
+		game.physics.arcade.collide(player, tree);
+		/* Collision between player and doors */
+		game.physics.arcade.collide(player, door1, test);
+		game.physics.arcade.collide(player, door2, test);
+		game.physics.arcade.collide(player, door3, test);
+		
+		/* Reset the player's velocity */
+		player.body.velocity.x = 0;
+		player.body.velocity.y = 0;
+
+		
+		/**********************************************/
+		/*************** Player controls **************/  
+		/**********************************************/
+		
+		/**************** Left or Right ***************/
+		if(cursors.left.isDown)
+		{
+			/* Move to the left */
+			player.body.velocity.x = -player_speed;
+			player.animations.play("left");
+		}
+		else if(cursors.right.isDown)
+		{
+			/* Move to the right */
+			player.body.velocity.x = player_speed;
+			player.animations.play("right");
+		}
+		/***************** Up or Down *****************/
+		else if(cursors.up.isDown)
+		{
+			/* Move to the right */
+			player.body.velocity.y = -player_speed;
+			player.animations.play("up");
+		}
+		else if(cursors.down.isDown)
+		{
+			/* Move to the right */
+			player.body.velocity.y = player_speed;
+			player.animations.play("down");
+		}
+		else
+		{
+			player.animations.stop();
+			player.frame = 0;
+		}
+	}
+	
+	function test(p, door)
+	{
+		player.kill();
+	}
 };
